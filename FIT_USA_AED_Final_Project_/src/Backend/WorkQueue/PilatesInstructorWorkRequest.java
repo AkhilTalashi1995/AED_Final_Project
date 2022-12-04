1267182b6c666ff7662db09fcd51d0d86e98e1f6
Aashay Pawar <pawar.aa@northeastern.edu>
2022-12-03 22:01:59 -0500

    PilatesInstructorWorkRequest.java

---
+++ FIT_USA_AED_Final_Project_/src/Backend/WorkQueue/PilatesInstructorWorkRequest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend.WorkQueue;
+
+/**
+ *
+ * @author Aashay
+ */
+public class PilatesInstructorWorkRequest extends WorkRequest {
+    
+    private String YogaInstructorAppointment;
+
+    public String getYogaInstructorAppointment() {
+        return YogaInstructorAppointment;
+    }
+
+    public void setYogaInstructorAppointment(String YogaInstructorAppointment) {
+        this.YogaInstructorAppointment = YogaInstructorAppointment;
+    }  
+    
+    
+}
