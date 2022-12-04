51ba2120d56083c530f6db1771dbfadd4bea4f6e
Aashay Pawar <pawar.aa@northeastern.edu>
2022-12-03 22:02:10 -0500

    SpaTherapyWorkRequest.java

---
+++ FIT_USA_AED_Final_Project_/src/Backend/WorkQueue/SpaTherapyWorkRequest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend.WorkQueue;
+
+/**
+ *
+ * @author Aashay
+ */
+public class SpaTherapyWorkRequest extends WorkRequest{
+    private String SpaAppointment;
+
+    public String getSpaAppointment() {
+        return SpaAppointment;
+    }
+
+    public void setSpaAppointment(String SpaAppointment) {
+        this.SpaAppointment = SpaAppointment;
+    }
+    
+}
