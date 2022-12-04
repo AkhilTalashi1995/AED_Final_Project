5b21602031699cc92222ab25b2e72304a6435bd4
Aashay Pawar <pawar.aa@northeastern.edu>
2022-12-03 22:02:22 -0500

    WorkRequest.java

---
+++ FIT_USA_AED_Final_Project_/src/Backend/WorkQueue/WorkRequest.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend.WorkQueue;
+
+import Backend.User.User;
+import java.util.Date;
+/**
+ *
+ * @author Mital
+ */
+public abstract class WorkRequest {
+
+    private String message;
+    private User sender;
+    private User receiver;
+    private String status;
+    private Date requestDate;
+    private Date resolveDate;
+    
+    public WorkRequest(){
+        requestDate = new Date();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Date getResolveDate() {
+        return resolveDate;
+    }
+
+    public void setResolveDate(Date resolveDate) {
+        this.resolveDate = resolveDate;
+    }    
+
+    @Override
+    public String toString(){
+        return this.message;
+    }    
+    
+}
