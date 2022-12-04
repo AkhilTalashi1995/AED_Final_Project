01977db5b97c49a016aac6cf99f8e0c7e9944adb
Aashay Pawar <pawar.aa@northeastern.edu>
2022-12-03 22:01:23 -0500

    BeauticianWorkRequest.java

---
+++ FIT_USA_AED_Final_Project_/src/Backend/WorkQueue/BeauticianWorkRequest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend.WorkQueue;
+
+/**
+ *
+ * @author Aashay
+ */
+public class BeauticianWorkRequest extends WorkRequest{
+    private String SalonAppointment;
+
+    public String getSalonAppointment() {
+        return SalonAppointment;
+    }
+
+    public void setSalonAppointment(String SalonAppointment) {
+        this.SalonAppointment = SalonAppointment;
+    }
+
+    
+}
