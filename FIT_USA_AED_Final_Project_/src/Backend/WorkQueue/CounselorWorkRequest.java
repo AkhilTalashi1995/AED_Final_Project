73627e8e81f29e3f5a6cf82c8c53c24a2738777b
Aashay Pawar <pawar.aa@northeastern.edu>
2022-12-03 22:01:30 -0500

    CounselorWorkRequest.java

---
+++ FIT_USA_AED_Final_Project_/src/Backend/WorkQueue/CounselorWorkRequest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend.WorkQueue;
+
+/**
+ *
+ * @author Aashay
+ */
+public class CounselorWorkRequest extends WorkRequest{
+    
+    private String TherapistAppointment;
+
+    public String getTherapistAppointment() {
+        return TherapistAppointment;
+    }
+
+    public void setTherapistAppointment(String TherapistAppointment) {
+        this.TherapistAppointment = TherapistAppointment;
+    }
+
+   
+    
+   
+}
