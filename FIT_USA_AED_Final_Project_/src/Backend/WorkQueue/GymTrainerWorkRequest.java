220bcb8504898b43416e0ef28cd2cb4d4cf9443c
Aashay Pawar <pawar.aa@northeastern.edu>
2022-12-03 22:01:44 -0500

    GymTrainerWorkRequest.java

---
+++ FIT_USA_AED_Final_Project_/src/Backend/WorkQueue/GymTrainerWorkRequest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend.WorkQueue;
+
+/**
+ *
+ * @author Akhil
+ */
+public class GymTrainerWorkRequest extends WorkRequest {
+    
+    private String GymInstructorAppointment;
+
+    
+    public String getGymInstructorAppointment() {
+        return GymInstructorAppointment;
+    }
+
+    public void setGymInstructorAppointment(String GymInstructorAppointment) {
+        this.GymInstructorAppointment = GymInstructorAppointment;
+    }  
+    
+}
